d6ebae12e508becec46b7f87f32ba02a116b0ff2
Vidish Naik <vidishnaik0509@gmail.com>
2019-10-07 23:33:29 -0700

    DoublyLinkedList delete and reverse complete

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -25,6 +25,12 @@ public class DoublyLinkedList {
             System.out.println("1. Insert Node in the end.");
             System.out.println("2. Insert Node at the start.");
             System.out.println("3. Insert Node at nth position.");
+            System.out.println("4. Delete Node at the end.");
+            System.out.println("5. Delete Node at the first position.");
+            System.out.println("6. Delete Node at Nth position");
+            System.out.println("7. Delete Node by value.");
+            System.out.println("8. Reverse the list using iteration.");
+            System.out.println("9. Reverse the list using recursion.");
             switch (sc.nextInt()) {
                 case 0:
                     printList();
@@ -47,6 +53,37 @@ public class DoublyLinkedList {
                         insertAtStart(val);
                     else
                         insertAtNth(val, pos - 1);
+                    printList();
+                    break;
+                case 4:
+                    deleteAtEnd();
+                    printList();
+                    break;
+                case 5:
+                    deleteAtStart();
+                    printList();
+                    break;
+                case 6:
+                    System.out.println("Enter the position of the node to delete.");
+                    pos = sc.nextInt();
+                    if(pos == 1)
+                        deleteAtStart();
+                    else
+                        deleteAtNth(pos - 1);
+                    printList();
+                    break;
+                case 7:
+                    System.out.println("Enter the value to be deleted.");
+                    val = sc.nextInt();
+                    deleteByValue(val);
+                    printList();
+                    break;
+                case 8:
+                    reverseByIteration();
+                    printList();
+                    break;
+                case 9:
+                    reverseByRecursion(head);
                     printList();
                     break;
                 default:
@@ -119,4 +156,103 @@ public class DoublyLinkedList {
         temp.next = newNode;
     }
 
+    private static void deleteAtEnd() {
+        if(head == null){
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.prev.next = null;
+        temp.prev = null;
+    }
+
+    private static void deleteAtStart() {
+        if(head == null){
+            return;
+        }
+        if(head.next == null){
+            head = null;
+            return;
+        }
+        head.next.prev = null;
+        head = head.next;
+    }
+
+    private static void deleteAtNth(int pos) {
+        if(head == null){
+            return;
+        }
+        Node temp = head;
+        while (pos > 1 && temp.next != null){
+            temp = temp.next;
+            pos -= 1;
+        }
+        if(temp.next == null || pos != 1){
+            System.out.println("Position is beyond the length of the list. Do you wish to delete the node at the end of the list?\n1. Yes\n2. No");
+            if (new Scanner(System.in).nextInt() == 1) {
+                temp.prev.next = null;
+                temp.prev = null;
+            }
+        }
+        else {
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+            temp.prev = null;
+            temp.next = null;
+        }
+
+    }
+
+    private static void deleteByValue(int val) {
+        Node temp = head;
+        while(temp != null){
+            if(temp.val == val){
+                if(temp.prev == null){
+                    head = temp.next;
+                    temp.next = null;
+                }
+                else if (temp.next == null){
+                    temp.prev.next = null;
+                    temp.prev = null;
+                }
+                else {
+                temp.next.prev = temp.prev;
+                temp.prev.next = temp.next;
+                temp.next = null;
+                temp.prev = null;
+                }
+                return;
+            }
+            temp = temp.next;
+        }
+        System.out.println(val + " not found in the list.");
+    }
+    private static void reverseByIteration() {
+        Node temp = head;
+        Node placeHolder = null;
+        while(temp.next != null){
+            placeHolder = temp.next;
+            temp.next = temp.prev;
+            temp.prev = placeHolder;
+            temp = temp.prev;
+        }
+        head = temp;
+        temp.next = temp.prev;
+        temp.prev = null;
+    }
+
+    private static void reverseByRecursion(Node current){
+        if(current.next == null){
+            head = current;
+            current.next = current.prev;
+            current.prev = null;
+            return;
+        }
+        reverseByRecursion(current.next);
+        Node placeHolder = current.next;
+        current.next = current.prev;
+        current.prev = placeHolder;
+    }
 }
